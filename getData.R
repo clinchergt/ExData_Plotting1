8580267d9393c780e4068ccbe30da92b91ef8efa
clinchergt <delvallejacobo@gmail.com>
2018-02-04 20:48:27 -0600

    Filter data

--- getData.R
+++ getData.R
@@ -2,4 +2,6 @@ download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_p
 unzip("zippy.zip", exdir="unzippy")
 setwd(paste0(getwd(), "/unzippy"))
 dataSet <- read.csv("household_power_consumption.txt", sep=";", na.strings = "?")
-
+dataSet$Date <- as.Date(strptime(dataSet$Date, format="%d/%m/%Y"))
+dataSet <- dataSet[dataSet$Date < as.Date(strptime("03/02/2007", format="%d/%m/%Y")) &
+                     dataSet$Date > as.Date(strptime("31/01/2007", format="%d/%m/%Y")),]
