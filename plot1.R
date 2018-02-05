55769ed61037215c91bf3d35f65a5e7eae7b66fe
clinchergt <delvallejacobo@gmail.com>
2018-02-04 20:34:48 -0600

    Get NA strings

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "zippy.zip")
 unzip("zippy.zip", exdir="unzippy")
 setwd(paste0(getwd(), "/unzippy"))
-dataSet <- read.csv("household_power_consumption.txt", sep=";")
+dataSet <- read.csv("household_power_consumption.txt", sep=";", na.strings = "?")
 
